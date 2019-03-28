45ab40a7689d248b1f49e02bddcf88efcf20b6a8
sechun <sechun0215@gmail.com>
2019-03-28 12:05:09 +0900

    initialize project

---
+++ src/main/java/com/assignment/rsupport/noticejava/NoticeJavaApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.rsupport.noticejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/rsupport/noticejava/NoticeJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assignment.rsupport.noticejava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoticeJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
